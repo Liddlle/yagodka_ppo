3c6866215fbf14d36f41b4f26428e30a3c8847de
liddlle <aabakhitova@vm-as337>
2017-02-10 11:51:32 +0300

    plots added

--- speed_date.R
+++ speed_date.R
@@ -6,9 +6,7 @@ Speed_Dating_Data <- read_csv("~/yagodka_ppo/Speed Dating Data.csv")
 uniq = Speed_Dating_Data %>% group_by(iid,age,gender) %>% tally()
 
 
-library(ggplot2)
-ggplot(data = uniq, aes(x = factor(age), fill=factor(gender)))+geom_bar()
-
-
 library(plotly)
-plot_ly(uniq, x = ~factor(age), color = ~factor(gender))
+plot_ly(uniq, x = ~factor(age), color = ~factor(gender), type = 'histogram', name = 'Age and gender') %>%
+ layout(yaxis = list(title = 'Count'), barmode = 'stack')
+
